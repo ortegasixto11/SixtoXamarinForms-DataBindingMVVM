52ab4cc3ed48477fe873c4888a0abc5a0f471dda
Sixto Ortega <sixto_miguel@hotmail.com>
2019-11-14 10:48:38 -0400

    Upload project

---
+++ DataBindingMVVM/DataBindingMVVM/DataBindingMVVM/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBindingMVVM
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    //[DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainViewModel MainViewModel { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MainViewModel = new MainViewModel();
+            BindingContext = MainViewModel;
+            MainViewModel.SetVisibility_LabelResult(false);
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LabelChangeVisibilityAsync();
+        }
+
+        public async Task<int> LabelChangeVisibilityAsync()
+        {
+            var result = await Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                MainViewModel.SetVisibility_LabelResult(true);
+                return 1;
+            });
+            return result;
+        }
+
+
+    }
+}
---
+++ DataBindingMVVM/DataBindingMVVM/DataBindingMVVM/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataBindingMVVM
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            this.LabelResultVisible = false;
+        }
+
+        private string myMessage;
+        public string MyMessage
+        {
+            get { return myMessage; }
+            set 
+            {
+                if (myMessage != value)
+                {
+                    myMessage = value; 
+                    OnPropertyChanged("MyMessage");
+                }
+            }
+        }
+
+        private bool labelResultVisible;
+        public bool LabelResultVisible
+        {
+            get { return labelResultVisible; }
+            set 
+            {
+                if (labelResultVisible != value)
+                {
+                    labelResultVisible = value;
+                    OnPropertyChanged("LabelResultVisible");
+                }
+            }
+        }
+
+        public void SetVisibility_LabelResult(bool visibility)
+        {
+            LabelResultVisible = visibility;
+        }
+
+    }
+}
